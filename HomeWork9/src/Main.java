3f5c8f7538ed6a42eac772da67933ba505a31be3
Donets-Yuliya <142098138+Donets-Yuliya@users.noreply.github.com>
2023-09-02 12:55:57 +0300

    Delete HomeWork9 directory

--- HomeWork9/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Files.Files;
-import LibraryPack.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Files.WriteFile("src\\Files\\Resourses"); //Задача 1
-        Library.Work(); //Задача 2
-    }
-}
