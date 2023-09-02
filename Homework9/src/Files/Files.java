994ec07ab6a0eb26f1613d42fd5c124223836953
Донец Юлия <www.meay.moor@gmail.com>
2023-09-02 12:59:01 +0300

    Домашнее задание 9

---
+++ Homework9/src/Files/Files.java
@@ -0,0 +1,39 @@
+package Files;
+
+import java.io.*;
+
+public class Files {
+
+    public static void WriteFile(String path) {
+        File catalog = new File(path);
+        File [] files = catalog.listFiles();
+        File result = new File(path + "\\result.txt");
+
+        result.delete();
+        try {
+            result.createNewFile();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path + "\\result.txt"))) {
+            for (File file : files) {
+                if (file.isFile() && !file.getName().equals("result.txt")) {
+
+                    try (BufferedReader br = new BufferedReader(new FileReader(file.getPath()))) {
+                        String s;
+                        while ((s = br.readLine()) != null) {
+                            bw.write(s);
+                        }
+                    }
+
+                }
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
